79433eff919ab056fcb15958f3a2a3565c1df036
EdgarVeliz13 <velizmej@gmail.com>
2023-06-26 20:31:54 -0300

    Arreglo de Menu Hamburguesa

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
-const menu = document.querySelector(".menu");
+// funcionalidad "Menu Hamburguesa"
+const menu = document.querySelector(".menu-conteiner");
 const openMenuBtn = document.querySelector(".open-menu");
 const closeMenuBtn = document.querySelector(".close-menu");
 
 function toggleMenu() {
-  menu.classList.toggle("menu_opened");
+  menu.classList.toggle("menu_abierto");
 }
 
 openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
 
+const enlaces = document.querySelectorAll(".menu-conteiner a");
 
+enlaces.forEach(enlace => {
+    enlace.addEventListener("click", function() {
+        menu.classList.remove("menu_abierto");
+    })
+})
+
+
+// Funcionalidad "Selector"
 let menuItem = document.querySelectorAll(".menu-item");
 menuItem.forEach(function (item){
      item.addEventListener("click", function(e){
@@ -19,23 +29,5 @@ menuItem.forEach(function (item){
 });
 });
 
-// const menuItems = document.querySelectorAll('.menu-item');
-// menuItems.forEach(function (el){
-//     el.addEventListener('click', function(e) {
-//         const currentItem = document.querySelector('.active');
-//         currentItem.classList.remove('active');
-//         e.target.classList.add('active');
-// });
-// });
 
-// const menu = document.querySelector(".menu");
-// const openMenu = document.querySelector(".open-menu");
-// const closeMenu = document.querySelector(".close-menu");
-
-// function toggleMenu() {
-//     menu.classList.toggle("menu_abierto");
-// }
-
-// openMenu.addEventListener("click", toggleMenu);
-// closeMenu.addEventListener("click", toggleMenu);
 
