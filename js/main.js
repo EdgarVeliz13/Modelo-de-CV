012c40b58753c99854f5d26044831cc313993c68
EdgarVeliz13 <velizmej@gmail.com>
2023-06-26 00:14:43 -0300

    Menu hamburguesa

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
+const menu = document.querySelector(".menu");
+const openMenuBtn = document.querySelector(".open-menu");
+const closeMenuBtn = document.querySelector(".close-menu");
+
+function toggleMenu() {
+  menu.classList.toggle("menu_opened");
+}
+
+openMenuBtn.addEventListener("click", toggleMenu);
+closeMenuBtn.addEventListener("click", toggleMenu);
+
+
 let menuItem = document.querySelectorAll(".menu-item");
 menuItem.forEach(function (item){
-    item.addEventListener("click", function(e){
-        const currentItem = document.querySelector(".active");
-        currentItem.classList.remove("active");
+     item.addEventListener("click", function(e){
+         const currentItem = document.querySelector(".active");
+         currentItem.classList.remove("active");
         e.target.classList.add("active");
-    });
+});
 });
 
 // const menuItems = document.querySelectorAll('.menu-item');
@@ -15,3 +27,15 @@ menuItem.forEach(function (item){
 //         e.target.classList.add('active');
 // });
 // });
+
+// const menu = document.querySelector(".menu");
+// const openMenu = document.querySelector(".open-menu");
+// const closeMenu = document.querySelector(".close-menu");
+
+// function toggleMenu() {
+//     menu.classList.toggle("menu_abierto");
+// }
+
+// openMenu.addEventListener("click", toggleMenu);
+// closeMenu.addEventListener("click", toggleMenu);
+
